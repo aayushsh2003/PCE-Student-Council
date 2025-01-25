904f15862eba1f52dfa2000b0437bf6943e87c38
Aayush Sharma <135792547+aayushsh2003@users.noreply.github.com>
2025-01-25 22:13:17 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import DeveloperModal from '@/components/DeveloperModal'
+import type { Metadata } from "next"
 
 const inter = Inter({ subsets: ['latin'] })
 
